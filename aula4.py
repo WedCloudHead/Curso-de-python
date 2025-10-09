f19032c861993c5cc547d192cc2e65c2ecea532a
Wederson da Silva Torres <wedersontorres15@gmail.com>
2025-10-09 16:09:55 -0300

    declarações basicas
    algumas declarações em python, importante ressaltar que é apenas um treino/aprendizado então possivelmente havera erros

---
+++ aula4.py
@@ -0,0 +1,15 @@
+{
+    print('hello, world!')
+
+}
+
+
+
+nome= input ('qual seu nome?')
+idade= input ('quantos anos voce tem?')
+peso= input ('qual o seu peso?')
+
+print(nome, idade, peso)         
+
+
+
---
+++ aula4exe.py
@@ -0,0 +1,40 @@
+#desafio 01
+#criar um script python que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado.
+
+nome= input ('Qual seu nome?')
+
+idade=input ('qual a sua idade?')
+
+
+{
+    print('ola '  + nome + ','  ' boas vindas ')
+
+}
+
+{
+    print(' voce tem ' +  idade + ' anos ')
+}
+
+
+#desafio 02
+#crie um script que leia o dia, mes e ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+
+dia=input (' qual dia do sue aniversário? ')
+
+mes=input (' qual mês do seu aniversário? ')
+
+ano=input (' qual ano do seu aniversário? ')
+
+{
+    print(' voce nasceu no dia ' + dia + ' do ' + mes + ' de ' + ano)
+}
+
+
+#desafio 03
+#crie um script que leia dois numeros e tente mostrar a soma entre eles.
+
+numero= (4 + 5)
+resultado= (numero)
+
+print(numero)
