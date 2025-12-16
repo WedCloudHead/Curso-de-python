b7226beb156b46c19f19b207eb064082b6edf49f
Wederson da Silva Torres <wedersontorres15@gmail.com>
2025-12-16 19:23:26 -0300

    Finalizações
    Finalização da sequencia de exercícios números 036 a 045, resoluções e correções!

--- exercicios-python/ex044.py
+++ exercicios-python/ex044.py
@@ -42,4 +42,37 @@ elif opções == 4:
 else:
     print('opção invalida, tente novamente!')
 
-    
+
+    #Como professor demonstrou:
+
+print('{:=^40}'.format('LOJAS GUANABARA'))
+preço = float(input('Preço das compras: R$'))
+print('''FORMAS DE PAGAMENTO
+[ 1 ] À vias dinheiro/ cheque
+[ 2 ] À vista cartão 
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opção = int(input('Qual é a opção? '))
+if opção == 1:
+    total = preço - (preço * 10 / 100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print(f'Sua compra será parcelada em 2x de R${parcela:.2f}')
+elif opção == 4:
+    total = preço + (preço * 20 / 100)
+    totparc = int(input('QUantas parcelas? '))
+    parcela = total / totparc
+    print(f'Sua compra será parcelada em {total:.2f}x de R${totparc:.2f} com juros')
+else:
+    total = 0
+    print('Opção inválida de pagamento. Tente novamente!')
+print(f'Sua compra de {preço:.2f} vai custar R${total:.2f} no final')
+
+
+#Anotações:
+#Ambos os códigos resolvem o problema, porém dando destaque a forma em que 
+#o professor desenvolve os cálculos diretamente nas condições enquanto eu 
+#criei uma variavel para cada cálculo totalmente desnecessários.
--- exercicios-python/ex045.py
+++ exercicios-python/ex045.py
@@ -1,2 +1,111 @@
 #Crie um programa que faça o computador jogar Jokenpô com você.
 
+#Como eu fiz:
+
+from random import randint 
+from time import sleep
+print('O FAMOSO JOGO DO JOKENPÔ VAI COMEÇAR, É SOMENTE VOCÊ CONTRA O COMPUTADOR!!!')
+print('1 = (PEDRA) 2 = (PAPEL) 3 = (TESOURA)')
+jogador = int(input('Escolha um dos três para disputar contra o computador: '))
+print('O computador já escolheu o dele tambem!')
+sleep(1)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ')
+sleep(0.8)
+
+computador = randint(1, 3)
+if jogador == 1 and computador == 2:
+    print('(PEDRA X PAPEL)')
+    print('O computador ganhou!!!')
+elif jogador == 2 and computador == 3:
+    print('(PAPEL X TESOURA)')
+    print('O computador ganhou!!!')
+elif jogador == 3 and computador == 1:
+    print('(TESOURA X PEDRA)')
+    print('O computador ganhou!!!')
+elif jogador == 1 and computador == 3:
+    print('PEDRA X TESOURA')
+    print('O jogador ganhou!!!')
+elif jogador == 2 and computador == 1:
+    print('(PAPEL X PEDRA)')
+    print('O jogador ganhou!!!')
+elif jogador == 3 and computador == 2:
+    print('(TESOURA X PAPEL)')
+    print('O jogador ganhou!!!')
+elif jogador == computador:
+    print('(HOUVE UM EMPATE!!!)')
+    print('Tente novamente.')
+else:
+    print('Essa é uma opção inválida, tente novamente!')
+    
+
+#Como professor demonstrou:
+
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+comp = randint(0, 2)
+#print('O computador escolheu {}'.format(itens[comp]))
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')  
+jog = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ!!!')
+print('-=' * 11)
+print('Computador jogou {}'.format(itens[comp]))
+print('Jogador jogou {}'.format(itens[jog]))
+print('-=' * 11)
+if comp == 0:
+    if jog == 0:
+        print('Empate')
+    elif jog == 1:
+        print('Jogador vence')
+    elif jog ==2:
+        print('Computador vence')
+    else:
+        print('Jogada inválida')
+
+elif comp == 1:
+    if jog == 1:
+        print('Empate')
+    elif jog == 2:
+        print('Jogador vence')
+    elif jog == 0:
+        print('Computador vence')
+    else:
+        print('Jogada inválida')
+
+
+elif comp == 2:
+    if jog == 2:
+        print('Empate')
+    elif jog == 0:
+        print('Jogador vence')
+    elif jog == 1:
+        print('Computador vence')
+    else:
+        print('Jogada inválida')
+
+
+
+
+
+#Anotações:
+#Eu estava em dúvida quando fiz meu programa se tinha alguma maneira de 
+#transformar o randint do import que é em números para letras e sim, o 
+#professor acabou de demonstrar na linha 50 com:
+#.format(intens[comp])
+#ele primeiro declara na linha 48 itens com 3 itens aleatórios e em 
+#seguida cria esse .format 
+#É possivel notar que nas linhas 63 e 64 é onde ocorre as transformações de
+#itens[] numeros para as letras, é o mesmo processo da linha 50 porém como
+#não tinha necessidade de deixar aquela linha no codigo eu só a tornei um 
+#comentário.
